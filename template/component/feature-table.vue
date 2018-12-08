fa0ef36996e015c3ba6af6a411d1ecae4d69c620
hao.zhang <735296233@qq.com>
2018-12-08 14:58:49 +0800

    :tada: create-project-template v1.0.0

---
+++ template/component/feature-table.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>feature-table</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'feature-table',
+  mixins: [],
+
+  data () {
+    return {}
+  },
+
+  props: {},
+
+  created () { },
+
+  mounted () { },
+
+  computed: {},
+
+  filters: {},
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+</style>
+
+
---
+++ template/config.js
@@ -0,0 +1,7 @@
+const params = {}
+
+const rule = {
+  name: [{ required: true, message: '请输入名称', trigger: 'blur' }]
+}
+
+export default { params, rule }
